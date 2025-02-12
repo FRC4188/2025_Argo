72d27223a9b1d62e08aa816cfc9d4b09cae1b2fb
Evans C <this0is0hard@gmail.com>
2025-02-11 19:07:26 -0500

    zawg

--- src/main/java/frc/robot/subsystems/scoring/Superstructure.java
+++ src/main/java/frc/robot/subsystems/scoring/Superstructure.java
@@ -1,7 +1,15 @@
 package frc.robot.subsystems.scoring;
+import java.time.LocalDate;
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.scoring.SuperstructureState.SuperPreset;
-import frc.robot.subsystems.scoring.Arm.Arm;
+import frc.robot.subsystems.scoring.arm.Arm;
 import frc.robot.subsystems.scoring.lift.Elevator;
 import frc.robot.subsystems.scoring.wrist.IntakeWrist;
 
@@ -9,17 +17,39 @@ public class Superstructure extends SubsystemBase{
     private final Arm arm;
     private final Elevator elevator;
     private final IntakeWrist wrist;
-    
-    private SuperPreset state = new SuperPreset.L4_CORAL;
-    private SuperPreset next = new SuperPreset();
-    private SuperPreset goal = new SuperPreset();
+
+    private final Constraints constraints = new Constraints(960.0, 720.0);
+
+    private ProfiledPIDController armPID = 
+        new ProfiledPIDController(
+            0.1, 0.0, 0.0, 
+            constraints);
+
+    private ProfiledPIDController wristPID = 
+        new ProfiledPIDController(
+            0.1, 0.0, 0.0, 
+            constraints);
+
+    private ProfiledPIDController elePID = 
+        new ProfiledPIDController(
+            0.1, 0.0, 0.0, 
+            constraints);
+        
+    private SuperPreset state =  SuperPreset.L4_CORAL;
     public Superstructure(Arm arm, Elevator elevator, IntakeWrist wrist){
         this.arm = arm;
         this.elevator = elevator;
         this.wrist = wrist;
     }
 
-   public void setgoal(){}
+   public void setgoal(SuperPreset goal){
+        var state = goal.getState();
+
+        arm.setVolt(
+            armPID.calculate(arm.getAngle(), state.getArmAngle())
+            + ArmFF.getArmVoltFF(VecBuilder.fill(arm.getAngle(), wrist.getMotorAngle()))
+        );
+   }
 
 
 
@@ -32,4 +62,4 @@ public class Superstructure extends SubsystemBase{
 
     
     
-}
+}//change
